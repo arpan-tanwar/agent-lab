585c8ca5be65ebc683dba90be78dcc33ac2793e9
Arpan Tanwar <arpantanwar.at@gmail.com>
2025-09-02 09:44:26 -0400

    chore: migrate to ESLint flat config (v9)

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import eslint from '@eslint/js';
+import ts from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  { ignores: ['dist', 'node_modules'] },
+  eslint.configs.recommended,
+  ...ts.configs.recommended,
+  prettier,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parser: ts.parser,
+      parserOptions: { project: false },
+    },
+    rules: {},
+  },
+];
